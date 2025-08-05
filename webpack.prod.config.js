0eaa589734bcd1a7426ca74cd347590275a4c8dd
RafyFH <rafyfakhrizal299@gmail.com>
2025-08-05 21:21:21 +0700

    wes

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,10 +1,14 @@
-module.exports = (env) => ({
+const { merge } = require('webpack-merge');
+const common = require('./webpack.config.js');
+const path = require('path');
+
+module.exports = merge(common, {
   mode: 'production',
-  devtool: false,
-  optimization: {
-    minimize: true,
-    splitChunks: {
-      chunks: 'all',
-    },
+  output: {
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
   },
+  devtool: 'source-map',
 });
